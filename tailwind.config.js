aba3d59b67647791be296ffc8f286ff8d761c1c0
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2020-09-22 12:36:18 +0200

    Add basic harness

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+const theme = require('tailwindcss/defaultTheme');
+const typography = require('@tailwindcss/typography');
+
+//const colorBrand = 'var(--color-pretty)';
+
+// Utils
+const round = (num) => num.toFixed(7).replace(/(\.[0-9]+?)0+$/, '$1').replace(/\.0$/, '');
+const rem = (px) => `${round(px / 16)}rem`;
+const em = (px, base) => `${round(px / base)}em`;
+const px = (px) => `${px}px`;
+
+module.exports = {
+	important: true, // See https://tailwindcss.com/docs/configuration#important
+	experimental: {
+		// See https://github.com/tailwindlabs/tailwindcss/pull/2159
+		applyComplexClasses: true
+	},
+	purge: {
+		enabled: process.env.HUGO_ENVIRONMENT === 'production',
+		content: [ './hugo_stats.json' ],
+		options: {
+			//whitelist: [ 'pl-1', 'pl-3' ],
+			defaultExtractor: (content) => {
+				let els = JSON.parse(content).htmlElements;
+				els = els.tags.concat(els.classes, els.ids);
+				return els;
+			}
+		}
+	},
+	plugins: [ typography ]
+};
