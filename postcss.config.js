aff6bf77afecee184e36b2c74f689f64a6448c54
Bjørn Erik Pedersen <bjorn.erik.pedersen@gmail.com>
2023-05-30 08:36:01 +0200

    Remove console.log

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,6 @@ let tailwindConfig = process.env.HUGO_FILE_TAILWIND_CONFIG_JS || './tailwind.con
 const tailwind = require('tailwindcss')(tailwindConfig);
 const autoprefixer = require('autoprefixer');
 
-console.log(`/* Using postcss.config.js from ${tailwindConfig} */`);
-
 module.exports = {
 	// eslint-disable-next-line no-process-env
 	plugins: [tailwind, ...(process.env.HUGO_ENVIRONMENT === 'production' ? [autoprefixer] : [])],
